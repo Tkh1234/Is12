d7ee8db233687dce5491cdfcd1373a22b10a4de9
Dz <zhenya-melnikov-1998@inbox.ru>
2023-09-20 20:08:35 +1000

    hw done

---
+++ src/Melnikov/Homework2/Exercise1.java
@@ -0,0 +1,15 @@
+package Melnikov.Homework2;
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите секунды: ");
+        long s = input.nextInt();
+        long sec = s % 60;
+        long min = (s / 60) % 60;
+        long hours = (s / 60) / 60;
+        System.out.println(hours + ":" + min + ":" + sec);
+    }
+}
