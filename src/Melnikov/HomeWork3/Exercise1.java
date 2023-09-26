97f024cb71ea613fc17b81c3cd86a0aa0af06555
Dz <zhenya-melnikov-1998@inbox.ru>
2023-09-26 21:17:53 +1000

    hw done

---
+++ src/Melnikov/HomeWork3/Exercise1.java
@@ -0,0 +1,73 @@
+package Melnikov.HomeWork3;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        byte b1 = 0b1100; // 2
+        byte b2 = 14; // 8
+        byte b3 = 12; // 10
+        byte b4 = 0xC; // 16
+
+        short s1 = -0b0101_0001_0100; // 2
+        short s2 = -2424; // 8
+        short s3 = -1300; // 10
+        short s4 = -0x514; // 16
+
+        int i1 = 0b0; // 2
+        int i2 = 0; // 8
+        int i3 = 0; // 10
+        int i4 = 0x0; // 16
+
+        long l1 = 0b0111_0101_1011_1100_1101_0001_0101L; // 2
+        long l2 = 726_746_425; // 8
+        long l3 = 123_456_789; // 10
+        long l4 = 0x75BCD15; // 16
+
+        float f1 = 6453.45617F;
+        float f2 = 7.849561f;
+
+        double d1 = 0.8453d;
+        double d2 = 781.18616;
+
+        boolean bo1 = true;
+        boolean bo2 = false;
+
+        char c1 = 'Ж';
+        char c2 = 1110;
+        char c3 = '\u2743';
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        System.out.println(bo1);
+        System.out.println(bo2);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+
+    }
+}
---
+++ src/Melnikov/HomeWork3/Exercise2.java
@@ -0,0 +1,20 @@
+package Melnikov.HomeWork3;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        char c1 = '\uD83D'; char c10 = '\uDC05';
+        char c2 = '\u2600';
+        char c3 = '\u03BE';
+        char c4 = '\u0392';
+        char c5 = '\u0393';
+        char c6 = '\u03BE';
+        char c7 = '\u0397';
+        char c8 = '\u0399';
+        char c9 = '\u03A5';
+
+        System.out.print(c1);System.out.println(c10);
+        System.out.println(c2);
+        System.out.print(c3);System.out.print(c4);System.out.print(c5);System.out.print(c6);System.out.print(c7);System.out.print(c8);System.out.print(c9);
+
+    }
+}
